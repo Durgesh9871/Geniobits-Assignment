df37e525968a08724bfc6152067e603b779c4292
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-21 12:45:10 +0530

    file

--- Frontend/my-app/src/Pages/ContactPage.jsx
+++ Frontend/my-app/src/Pages/ContactPage.jsx
@@ -1,9 +1,16 @@
 import { Box, Input, Textarea } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 const ContactPage = () => {
+  const [state , setState] = useState({
+    to:"" ,
+    from :"" ,
+    text :"",
+  })
+
+
 
   const image = "https://media.istockphoto.com/id/1323765737/photo/close-up-of-a-businessman-using-a-laptop-computer-and-a-mobile-phone.jpg?s=612x612&w=0&k=20&c=a2jwh-IYmqgEYQKnzIoEncbNHISN2m2dAgP3JyMq_J8="
     
@@ -13,11 +20,11 @@ const ContactPage = () => {
  {/* contact -----input for forms --------- */}
       <Box  shadow="2xl" background="#27272a" borderRadius="8px"  border="2px  #ffffff" height="auto" width={{base:"90%", sm: "65%", md: "50%", lg: "35%",xl: "35%",'2xl': "35%"}} p="20px" position="relative" left="5%"  top="25%">
   
-  <Input placeholder='Type Receiver Email' width="90%"  focusBorderColor="white"  m="20px 0px" color="white"  />
+  <Input placeholder='Type Receiver Email' width="90%"  focusBorderColor="white"  m="20px 0px" color="white"  value={state.to} />
 
-  <Input placeholder='Type Sender Email' width="90%"  focusBorderColor="white" m="20px 0px" color="white" />
+  <Input placeholder='Type Sender Email' width="90%"  focusBorderColor="white" m="20px 0px" color="white" value={state.from} />
 
-  <Textarea placeholder='Type your message...' width="90%" focusBorderColor="white" m="30px 0px" color="white" />
+  <Textarea placeholder='Type your message...' width="90%" focusBorderColor="white" m="30px 0px" color="white" value={state.text}  />
 
       </Box>
     </Box>
