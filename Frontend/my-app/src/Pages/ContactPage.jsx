f63013169a48a1bb1817c0aa995479fd3d688efb
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-21 17:44:56 +0530

    file

--- Frontend/my-app/src/Pages/ContactPage.jsx
+++ Frontend/my-app/src/Pages/ContactPage.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, FormControl, Input, Textarea } from '@chakra-ui/react'
 import React, { useState } from 'react'
-
+import { useToast } from '@chakra-ui/react'
 
 
 const ContactPage = () => {
@@ -18,14 +18,31 @@ const ContactPage = () => {
    })
   }
 // for sending email --------------
+// 4df665f5-9ae8-4d44-95c6-c5b2bce25b5a -- token we get from smptjs.com
+ 
 
 const handleForm = ()=>{
-  console.log(state)
-  setState({
-    to:"" ,
-    from :"",
-    text:""
-  })
+console.log(state ,"satte")
+  const config = {
+    SecureToken : "4df665f5-9ae8-4d44-95c6-c5b2bce25b5a",
+     To :  state.to ,
+    From : "devisingh9871686098@gmail.com" ,
+    Subject : "Interested in this property",
+    Body : state.text
+   }
+console.log(config ,"config")
+  if(window.Email){
+    window.Email.send(config).then(()=> alert('Email Sent')).then(()=> {
+      setState({
+        to:"" ,
+        from :"",
+        text:""
+      })
+    })
+  }
+
+
+  
 }
 
   const image = "https://media.istockphoto.com/id/1323765737/photo/close-up-of-a-businessman-using-a-laptop-computer-and-a-mobile-phone.jpg?s=612x612&w=0&k=20&c=a2jwh-IYmqgEYQKnzIoEncbNHISN2m2dAgP3JyMq_J8="
@@ -46,7 +63,7 @@ const handleForm = ()=>{
   <Textarea placeholder='Type your message...' width="100%" focusBorderColor="white" m="30px 0px" color="white" name="text" value={state.text} onChange={handleChange} />
    </FormControl>
 
-   <Button colorScheme='facebook'  m="30px 0px" width="100%" onClick={handleForm}>Send </Button>
+   <Button colorScheme='facebook' isDisabled={state.to == "" || state.text == ""}  m="30px 0px" width="100%" onClick={handleForm}>Send </Button>
     {/* <input type="submit" /> */}
       </Box>
     </Box>
