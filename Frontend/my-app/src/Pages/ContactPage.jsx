f0c55e8ad79018827e0884caefca5c61eb9d784c
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-21 12:50:34 +0530

    file

--- Frontend/my-app/src/Pages/ContactPage.jsx
+++ Frontend/my-app/src/Pages/ContactPage.jsx
@@ -9,8 +9,15 @@ const ContactPage = () => {
     from :"" ,
     text :"",
   })
-
-
+ 
+  const handleChange = (e)=>{
+   const {name , value} = e.target 
+   setState({
+    ...state  , 
+    [name]:value 
+   })
+  }
+// console.log(state ,"state")
 
   const image = "https://media.istockphoto.com/id/1323765737/photo/close-up-of-a-businessman-using-a-laptop-computer-and-a-mobile-phone.jpg?s=612x612&w=0&k=20&c=a2jwh-IYmqgEYQKnzIoEncbNHISN2m2dAgP3JyMq_J8="
     
@@ -20,11 +27,11 @@ const ContactPage = () => {
  {/* contact -----input for forms --------- */}
       <Box  shadow="2xl" background="#27272a" borderRadius="8px"  border="2px  #ffffff" height="auto" width={{base:"90%", sm: "65%", md: "50%", lg: "35%",xl: "35%",'2xl': "35%"}} p="20px" position="relative" left="5%"  top="25%">
   
-  <Input placeholder='Type Receiver Email' width="90%"  focusBorderColor="white"  m="20px 0px" color="white"  value={state.to} />
+  <Input placeholder='Type Receiver Email' width="90%"  focusBorderColor="white"  m="20px 0px" color="white" name="to"  value={state.to} onChange={handleChange} />
 
-  <Input placeholder='Type Sender Email' width="90%"  focusBorderColor="white" m="20px 0px" color="white" value={state.from} />
+  <Input placeholder='Type Sender Email' width="90%"  focusBorderColor="white" m="20px 0px" color="white" name="from" value={state.from} onChange={handleChange} />
 
-  <Textarea placeholder='Type your message...' width="90%" focusBorderColor="white" m="30px 0px" color="white" value={state.text}  />
+  <Textarea placeholder='Type your message...' width="90%" focusBorderColor="white" m="30px 0px" color="white" name="text" value={state.text} onChange={handleChange} />
 
       </Box>
     </Box>
